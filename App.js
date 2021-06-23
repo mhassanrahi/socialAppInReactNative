760f55b3b469b9fe94f3da90bf04ac6b920ca83d
Mehmood <m.hassan.rahi@gmail.com>
2021-06-23 23:08:14 +0500

    * Updated App.js file

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+
+import Navigator from './src/navigation';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Hello App!</Text>
-    </View>
-  );
+  return <Navigator />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
